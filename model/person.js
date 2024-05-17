34148b5074b5b44bbc253cc828e8223b8b59820e
Abhay <abhayagarwal70371@gmail.com>
2024-05-18 02:10:46 +0530

    update person schema

--- model/person.js
+++ model/person.js
@@ -6,7 +6,7 @@ const personSchema = new mongoose.Schema({
     username:{
         type :String,
         required:true,
-        unique: true
+         unique: true
     },
     password:{
         type :String,
